1230fe6cf6e256363179ee6abc78734479bdd8d0
Jonas Franke <jonas.franke1@gmx.net>
2023-02-05 14:39:11 +0100

    add ping cmd

---
+++ src/main/java/de/j/stationofdoom/cmd/PingCommand.java
@@ -0,0 +1,18 @@
+package de.j.stationofdoom.cmd;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class PingCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player player) {
+            player.sendMessage(ChatColor.LIGHT_PURPLE + "Dein Ping ist " + player.getPing() + "ms");
+        }
+        return false;
+    }
+}
