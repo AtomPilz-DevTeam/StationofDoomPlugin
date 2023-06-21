34080e34d67fafbbf7bd80c9be5d970ca12dab87
Jonas Franke <jonas.franke1@gmx.net>
2023-06-21 21:56:07 +0200

    rm shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("xyz.jpenilla.run-paper") version "2.1.0"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
     id("io.papermc.paperweight.userdev") version "1.5.5"
