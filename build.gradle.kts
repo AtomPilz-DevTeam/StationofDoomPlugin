d3cbb58e7e2a3e20b690326783a3a087c7bdf8b7
Jonas Franke <jonas.franke1@gmx.net>
2023-10-22 14:34:54 +0200

    add run number to version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 val minecraftVersion = "1.20"
-val pluginVersion = "1.12.3"
+val pluginVersion: String = "1.12.3" + if (System.getenv("runnumber") != null) "." + System.getenv("pluginversion") else ""
 
 group = "org.example"
 version = pluginVersion
