d1cd48851e607d8d116f63fa50f6ee8a5108fb11
Jonas Franke <info@jonasfranke.xyz>
2024-10-03 11:36:34 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 val minecraftVersion = "1.21.1"
-val pluginVersion: String = "1.14.2" + if (System.getenv("runnumber") != null) "." + System.getenv("runnumber") else ""
+val pluginVersion: String = "1.14.2.1" + if (System.getenv("runnumber") != null) "." + System.getenv("runnumber") else ""
 
 group = "org.example"
 version = pluginVersion
