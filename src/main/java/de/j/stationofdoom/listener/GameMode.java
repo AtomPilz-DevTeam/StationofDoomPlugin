53cf565b8f1a205798f14bdadafbf36fb12638d9
Luca Weimer <luca.weimer.42@gmail.com>
2024-11-03 21:18:28 +0100

    Revert "deleted file hindering changing gamemode"
    This reverts commit 7ccbac894f2f1fe38088a68967f9008e93042e01.

---
+++ src/main/java/de/j/stationofdoom/listener/GameMode.java
@@ -0,0 +1,14 @@
+package de.j.stationofdoom.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+
+public class GameMode implements Listener {
+
+    @EventHandler
+    public void onGmchange(PlayerGameModeChangeEvent event){
+        event.getPlayer().setGameMode(org.bukkit.GameMode.SURVIVAL);
+        event.setCancelled(true);
+    }
+}
