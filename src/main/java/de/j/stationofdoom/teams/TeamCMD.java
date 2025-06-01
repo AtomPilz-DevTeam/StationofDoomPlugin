39d56e4ec8500de39bc2e537528819576ff4b3eb
Luca Weimer <125275369+PrinzPizza42@users.noreply.github.com>
2025-06-01 19:23:40 +0200

    Update src/main/java/de/j/stationofdoom/teams/TeamCMD.java
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/main/java/de/j/stationofdoom/teams/TeamCMD.java
+++ src/main/java/de/j/stationofdoom/teams/TeamCMD.java
@@ -1,32 +1,14 @@
-package de.j.stationofdoom.teams;
-
-import de.j.deathMinigames.main.HandlePlayers;
-import de.j.stationofdoom.util.translations.TranslationFactory;
-import io.papermc.paper.command.brigadier.BasicCommand;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-
-public class TeamCMD implements BasicCommand {
-    public TeamsMainMenuGUI teamsMainMenuGUI = new TeamsMainMenuGUI();
-
-    @Override
-    public boolean canUse(@NotNull CommandSender sender) {
-        return sender instanceof Player;
-    }
-
     @Override
     public void execute(CommandSourceStack stack, String[] args) {
-        Player player = (Player) stack.getSender();
++       if (!(stack.getSender() instanceof Player player)) {
++           return;
++       }
+-       Player player = (Player) stack.getSender();
         Team team = HandleTeams.getTeamFromPlayerUUID(player.getUniqueId());
-        if(!team.isMember(player.getUniqueId())) {
-            player.sendMessage(Component.text(new TranslationFactory().getTranslation(player, "TeamCMDNoTeam")).color(NamedTextColor.RED));
+        if (!team.isMember(player.getUniqueId())) {
+            player.sendMessage(Component.text(new TranslationFactory().getTranslation(player, "TeamCMDNoTeam"))
+                                         .color(NamedTextColor.RED));
             return;
         }
         new TeamSettingsGUI(team).showPage(1, player);
     }
-}
